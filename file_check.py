8174e55d1ff0dd45b593138b58fe3828af5ae993
demon-o5 <129477612+demon-o5@users.noreply.github.com>
2024-06-09 22:57:15 +0530

    Add files via upload
    file_check.py added

---
+++ file_check.py
@@ -0,0 +1,33 @@
+import os
+import datetime
+
+def create_file(filename):
+    try:
+        with open(filename,'w') as f:
+            f.write("#new file created")
+        print(f'{filename} \t new file is created')
+    except Exception as e:
+        print("unable to create the file")
+
+def remove_file_name(filename):
+    '''this function is used to check and remove the file '''
+    if os.path.exists(filename):
+        if os.path.getsize(filename) !=0:
+            #raise ValueError(f"file is not empty {filename}")
+            f=open(filename,'r')
+            print(f.read())
+        else:
+            os.remove(filename)
+    else:
+        print(f"file not exist {filename}")
+
+def rename_file(filename,new_name=None):
+    '''rename the file'''
+    base, ext = os.path.splitext(filename) # split file and extension and adding date to file name
+    new_name = f"{base}_{datetime.date.today()}{ext}"
+    os.rename(filename,new_name)
+    print(f"File renamed to {new_name}")
+
+#m=create_file('file_2.txt')
+rename_file('file_1.txt')
+    
